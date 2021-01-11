f2b6ae6277b20220116d282b18de836efb3eec0f
zcake <zcake@yahoo.com>
2021-01-11 20:29:39 +0800

    make pluseaduio autostart

--- artix.sh
+++ artix.sh
@@ -30,6 +30,7 @@ bin=start-artix.sh
 echo "writing launch script"
 cat > $bin <<- EOM
 #!/bin/bash
+pulseaudio --start &
 cd \$(dirname \$0)
 ## unset LD_PRELOAD in case termux-exec is installed
 unset LD_PRELOAD
